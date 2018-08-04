6cb9020dba2963cf88fda4dabcced6b305e794fd
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-08-04 14:33:23 -0500

    Add QtWebEngineApplication example

---
+++ QtWebEngineApplication/QtWebEngineApplication.pro
@@ -0,0 +1,15 @@
+TEMPLATE = app
+
+QT += webengine
+
+SOURCES += main.cpp
+
+RESOURCES += qml.qrc
+
+target.path = $$PREFIX/bin
+icon.path = $$PREFIX/share/icons/hicolor/512x512
+icon.files = QtWebEngineApplication.png
+
+desktop_entry.path = $$PREFIX/share/applications
+desktop_entry.files = QtWebEngineApplication.desktop
+INSTALLS += target icon desktop_entry
--- build_appimages.sh
+++ build_appimages.sh
@@ -6,11 +6,11 @@ BASE_DIR=${PWD}
 mkdir ${BASE_DIR}/out
 
 # Download tools
-wget -N https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-"$ARCH".AppImage
-chmod +x linuxdeploy-"$ARCH".AppImage
+#wget -N https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-"$ARCH".AppImage
+#chmod +x linuxdeploy-"$ARCH".AppImage
 
-wget -N https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-"$ARCH".AppImage
-chmod +x linuxdeploy-plugin-qt-"$ARCH".AppImage
+#wget -N https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-"$ARCH".AppImage
+#chmod +x linuxdeploy-plugin-qt-"$ARCH".AppImage
 
 LINUXDEPLOY_BIN=${PWD}/linuxdeploy-"$ARCH".AppImage
 LINUXDEPLOY_PLUGIN_QT_BIN=${PWD}/linuxdeploy-plugin-qt-"$ARCH".AppImage
@@ -25,18 +25,36 @@ build_appimage() {
     ${LINUXDEPLOY_PLUGIN_QT_BIN} --appdir ${APP_DIR} || return 1
     ${LINUXDEPLOY_BIN} --app-name ${APP_NAME} --appdir ${APP_DIR} --output appimage || return 1
 
-    mv *.AppImage ${BASE_DIR}/out || return 1
+    mv -v *AppImage ${BASE_DIR}/out || return 1
 }
 
-# Build projects
+ Build projects
 pushd QtQuickControls2Application
+    # This env variable is used by the qt plugin to search the qml sources in other paths than the AppDir
+    # it's mandatory to use when your qml files are embed as Qt resources into the main binary.
     export QML_SOURCES_PATHS="${PWD}/src"
+
+    # This env variable is used by the qt plugin to search the qml modules in other paths than the default
+    # Qt qml dir.
+    export QML_MODULES_PATHS=""
 
     mkdir build
     pushd build
-        cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr
-        DESTDIR=AppDir make install
+        cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr || exit 1
+        DESTDIR=AppDir make install || exit 1
 
-        build_appimage QtQuickControls2Application ${PWD}/AppDir
+        build_appimage QtQuickControls2Application ${PWD}/AppDir || exit 1
+    popd
+popd
+
+pushd QtWebEngineApplication
+    export QML_SOURCES_PATHS="${PWD}"
+
+    mkdir build
+    pushd build
+        qmake CONFIG+=release PREFIX=/usr ../QtWebEngineApplication.pro || exit 1
+        INSTALL_ROOT=${PWD}/AppDir make install || exit 1
+
+        build_appimage QtWebEngineApplication ${PWD}/AppDir || exit 1
     popd
 popd
