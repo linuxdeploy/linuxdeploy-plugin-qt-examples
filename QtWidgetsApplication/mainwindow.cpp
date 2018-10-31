261f12d1392cae986d04bb2a572965cf3744b7eb
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-10-31 11:35:06 -0600

    Add action whith icon which is loaded from the system theme

--- QtWidgetsApplication/mainwindow.cpp
+++ QtWidgetsApplication/mainwindow.cpp
@@ -6,6 +6,16 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QAction *action = new QAction(this);
+    QIcon icon;
+    auto iconThemeName = QStringLiteral("list-add");
+    if (QIcon::hasThemeIcon(iconThemeName)) {
+        icon = QIcon::fromTheme(iconThemeName);
+    } else {
+        icon.addFile(QStringLiteral("."), QSize(), QIcon::Normal, QIcon::Off);
+    }
+    action->setIcon(icon);
+    ui->mainToolBar->addAction(action);
 }
 
 MainWindow::~MainWindow()
